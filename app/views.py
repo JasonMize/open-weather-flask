c6e0aa754d7b4cadd44d7b1117244f5b30b8386a
Matt Smith <msmith@apaxsoftware.com>
2016-09-10 12:27:58 -0400

    Hello world HTML.

--- app/views.py
+++ app/views.py
@@ -3,4 +3,23 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World"
+    user = {
+        "full_name": "Matt Smith",
+        "username": "mattsmith1",
+    }
+
+
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello {full_name}</h1>
+    <p>
+        Your username is {username}
+</body>
+</html>
+""".format(full_name=user["full_name"],
+    username=user["username"])
