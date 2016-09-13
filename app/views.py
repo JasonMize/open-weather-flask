e273473b25d192eb8df46830a9f6571569cc36c2
Jason Mize <jasonemize@gmail.com>
2016-09-13 10:40:43 -0400

    setup routes

--- app/views.py
+++ app/views.py
@@ -5,6 +5,12 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-  
-
     return render_template("index.html")
+
+@app.route("/current")
+def current_weather ():
+    return render_template('current.html')
+
+@app.route("/forecast")
+def forecast_weather ():
+    return render_template('forecast.html')
